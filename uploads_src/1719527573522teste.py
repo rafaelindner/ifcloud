5d7bb2d2c2c0f030d54cfce5a276e1b38534627c
JeremiasPiontkoski <jeremiaspion@gmail.com>
2024-09-02 20:36:02 -0300

    Add .env.example

--- uploads_src/1719527573522teste.py
+++
@@ -1,8 +0,0 @@
-
-from helpers.file_utils import read_params_file
-
-params_file = sys.argv[1]
-data = read_params_file(params_file)
-
-print(data[:100])
-    
--- uploads_src/aa.py
+++
@@ -1,6 +0,0 @@
-import biosppy.signals.ecg as ecg
-from helpers.file_utils import read_params_file
-
-if __name__ == "__main__":
-    str = read_params_file()
-    print(str)
